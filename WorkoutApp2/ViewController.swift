932deda1c0b1f84254d13db42501c18738147e68
Ryan Deathridge <ryandeathridge@gmail.com>
2018-07-07 22:26:59 +1000

    added Squat exercise. previous and next buttons continue indefinitely

--- WorkoutApp2/ViewController.swift
+++ WorkoutApp2/ViewController.swift
@@ -11,44 +11,98 @@ import CoreData
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var exerciseLabel: UILabel!
     @IBOutlet weak var todaysLiftLabel: UITextField!
     @IBOutlet weak var lastLiftLabel: UILabel!
+    @IBOutlet weak var nextLiftLabel: UILabel!
+    @IBOutlet weak var incrementsLabel: UITextField!
     
-    var lastLift: Float = 102
-    var todaysLift: Float = 106
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    
+    var lastLift: Float = 100
+    var todaysLift: Float = 105
     var nextLift: Float = 112
+    var increment: Float = 5
+    var exercise: String = "Bench"
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        saveData()
+        initialiseDatabase()
+        requestData()
     }
 
-   
-    func saveData() {
+    
+    func initialiseDatabase() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-  
-        let entity = NSEntityDescription.entity(forEntityName: "Bench", in: context)
-        let newUser = NSManagedObject(entity: entity!, insertInto: context)
         
-        newUser.setValue("bench", forKey: "name")
-        newUser.setValue(80, forKey: "score")
-        newUser.setValue(4, forKey: "increment")
+        let entity1 = NSEntityDescription.entity(forEntityName: "Bench", in: context)
+        let entity2 = NSEntityDescription.entity(forEntityName: "Heave", in: context)
+        let entity3 = NSEntityDescription.entity(forEntityName: "Press", in: context)
+        let entity4 = NSEntityDescription.entity(forEntityName: "Squat", in: context)
+        let benchData = NSManagedObject(entity: entity1!, insertInto: context)
+        let heaveData = NSManagedObject(entity: entity2!, insertInto: context)
+        let pressData = NSManagedObject(entity: entity3!, insertInto: context)
+        let squatData = NSManagedObject(entity: entity4!, insertInto: context)
+        
+        
+        benchData.setValue("Bench", forKey: "name")
+        benchData.setValue(100, forKey: "score")
+        benchData.setValue(5, forKey: "increment")
+        
+        heaveData.setValue("Heave", forKey: "name")
+        heaveData.setValue(40, forKey: "score")
+        heaveData.setValue(2.5, forKey: "increment")
+        
+        pressData.setValue("Press", forKey: "name")
+        pressData.setValue(68, forKey: "score")
+        pressData.setValue(2.5, forKey: "increment")
+        
+        squatData.setValue("Squat", forKey: "name")
+        squatData.setValue(60, forKey: "score")
+        squatData.setValue(10, forKey: "increment")
         
         do {
             try context.save()
         } catch {
             print("failed to save")
         }
-        populateFields()
+    }
+    
+   
+    func saveData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+  
+        let entity = NSEntityDescription.entity(forEntityName: exercise, in: context)
+        let newData = NSManagedObject(entity: entity!, insertInto: context)
+        
+        
+        newData.setValue(exercise, forKey: "name")
+        newData.setValue(todaysLift, forKey: "score")
+        newData.setValue(increment, forKey: "increment")
+        
+        do {
+            try context.save()
+        } catch {
+            print("failed to save")
+        }
+        requestData()
+        
     }
     
     
     func populateFields() {
-        //code
+        
         todaysLiftLabel.text = "\(todaysLift)"
-        requestData()
+        lastLiftLabel.text = "\(lastLift)"
+        nextLiftLabel.text = "\(nextLift)"
+        exerciseLabel.text = exercise.uppercased()
+        incrementsLabel.text = "\(increment)"
+        
     }
     
     
@@ -57,25 +111,87 @@ class ViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Bench")
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: exercise)
         fetchRequest.returnsObjectsAsFaults = false
         
         do {
             let result = try context.fetch(fetchRequest)
             for data in result as! [NSManagedObject] {
-                print(data.value(forKey: "name") as! String)
+                if data.value(forKey: "name") == nil {
+                    initialiseDatabase()
+                } else {
+            
+               // exercise = data.value(forKey: "name") as! String
+                increment = data.value(forKey: "increment") as! Float
+                lastLift = data.value(forKey: "score") as! Float
+                }
             }
         } catch {
             print("failed")
         }
-        lastLiftLabel.text = "\(lastLift)"
         
+        todaysLift = lastLift + increment
+        nextLift = todaysLift + increment
+        
+        populateFields()
     }
     
     
+    @IBAction func doneButtonWasPressed(_ sender: Any) {
+        todaysLift = (todaysLiftLabel.text! as NSString).floatValue
+        increment = (incrementsLabel.text! as NSString).floatValue
+        saveData()
+        view.endEditing(true)
+    }
+    
+    @IBAction func nextButtonWasPressed(_ sender: Any) {
+        if exercise == "Bench" {
+            exercise = "Press"
+            previousButton.setTitle("Bench", for: UIControlState.normal)
+            nextButton.setTitle("Squat", for: UIControlState.normal)
+            
+        } else if exercise == "Press" {
+            exercise = "Squat"
+            nextButton.setTitle("Heave", for: UIControlState.normal)
+            previousButton.setTitle("Press", for: UIControlState.normal)
+        
+        } else if exercise == "Squat"{
+            exercise = "Heave"
+            nextButton.setTitle("Bench", for: UIControlState.normal)
+            previousButton.setTitle("Squat", for: UIControlState.normal)
+        } else if exercise == "Heave"{
+            exercise = "Bench"
+            nextButton.setTitle("Press", for: UIControlState.normal)
+            previousButton.setTitle("Heave", for: UIControlState.normal)
+        }
+        requestData()
+    }
+    
+   
     
     
-    
+    @IBAction func PreviousButtonPressed(_ sender: Any) {
+        if exercise == "Bench" {
+            exercise = "Heave"
+            nextButton.setTitle("Bench", for: UIControlState.normal)
+            previousButton.setTitle("Squat", for: UIControlState.normal)
+            
+        } else if exercise == "Heave" {
+            exercise = "Squat"
+            nextButton.setTitle("Heave", for: UIControlState.normal)
+            previousButton.setTitle("Press", for: UIControlState.normal)
+            
+        } else if exercise == "Press" {
+            exercise = "Bench"
+            nextButton.setTitle("Press", for: UIControlState.normal)
+            previousButton.setTitle("Heave", for: UIControlState.normal)
+        } else if exercise == "Squat" {
+            exercise = "Press"
+            nextButton.setTitle("Squat", for: UIControlState.normal)
+            previousButton.setTitle("Bench", for: UIControlState.normal)
+        }
+       requestData()
+    }
     
     
 }
